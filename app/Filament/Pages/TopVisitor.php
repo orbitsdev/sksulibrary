37ca7ac9df61746d6904dd81b66b44c864193af6
orbitsdev <orbinobrian0506@example.com>
2023-09-18 20:23:12 +0800

    wip

--- app/Filament/Pages/TopVisitor.php
+++ app/Filament/Pages/TopVisitor.php
@@ -9,6 +9,7 @@ use App\Models\Student;
 use Filament\Pages\Page;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\DateTimePicker;
 
 class TopVisitor extends Page implements Forms\Contracts\HasForms 
 {
@@ -19,7 +20,8 @@ class TopVisitor extends Page implements Forms\Contracts\HasForms
     protected static string $view = 'filament.pages.top-visitor';
 
     public $students = [];
-    public $month_selected;
+    public $from;
+    public $to;
     public $course_selected;
     public $year_selected;
     public $campus_selected;
@@ -34,8 +36,16 @@ class TopVisitor extends Page implements Forms\Contracts\HasForms
         ->orderByDesc('logins_count')
         ->take(10) // Get the top 10 students
         ->get();
+
+        //  dd($this->students);
+
+        // dd($this->students);
         
         
+    }
+
+    public function fileterQuery(){
+
     }
     protected function getFormSchema(): array 
     {
@@ -43,7 +53,7 @@ class TopVisitor extends Page implements Forms\Contracts\HasForms
             Grid::make(6)
             ->schema([
           
-             
+          
                
                 Select::make('course_selected')
                     ->options(collect(Course::query() // Filter courses based on the selected campus.
@@ -56,10 +66,78 @@ class TopVisitor extends Page implements Forms\Contracts\HasForms
                     ->label('Course')
                     ->reactive()
                     ->afterStateUpdated(function (Closure $set, $state) {
+                        $this->students = Student::whereHas('logins', function ($query) {
+                            $query->whereMonth('created_at', now()->month); // Filter for the current month
+                        })
+                        ->withCount(['logins' => function ($query) {
+                            $query->whereMonth('created_at', now()->month); // Filter for the current month
+                        }])
+                        ->when($state != 'all', function($query) use ($state){
+                            $query->whereHas('course', function($query) use($state){
+                                $query->where('id', $state);
+                            });
+                        })
 
+                        ->when($this->from || $this->to, function ($query) {
+                         
+                            $query->whereHas('login', function ($query) {
+                                if ($this->from && $this->to) {
+                                    $from = $this->from . ' 00:00:00'; // Start of selected day
+                                    $to = $this->to . ' 23:59:59';    
+                                    $query->whereBetween('created_at', [$from, $to]);
+                                  
+                                } elseif ($this->from) {
+                                    
+                                    $query->where('created_at', '>=', $this->from);
+                                    
+                                } elseif ($this->to) {
+                                    $query->where('created_at', '<=', $this->to);
+                                }
+                            });
+                        })
+                        ->orderByDesc('logins_count')
+                        ->take(10) // Get the top 10 students
+                        ->get();
                      
                         
                     }),
+
+                    DateTimePicker::make('from')->label('From')
+                    ->reactive()
+                    ->afterStateUpdated(function (Closure $set, $state) {
+                        $this->students = Student::whereHas('logins', function ($query) {
+                            $query->whereMonth('created_at', now()->month); // Filter for the current month
+                        })
+                        ->withCount(['logins' => function ($query) {
+                            $query->whereMonth('created_at', now()->month); // Filter for the current month
+                        }])
+                        ->when($state != 'all', function($query) use ($state){
+                            $query->whereHas('course', function($query) use($state){
+                                $query->where('id', $state);
+                            });
+                        })
+
+                        ->when($state || $this->to, function ($query)  use ($state){
+                         
+                            $query->whereHas('logins', function ($query) use($state) {
+                                if ($state && $this->to) {
+                                    $from = $state . ' 00:00:00'; // Start of selected day
+                                    $to = $this->to . ' 23:59:59';    
+                                    $query->whereBetween('created_at', [$from, $to]);
+                                  
+                                } elseif ($state) {
+                                    
+                                    $query->where('created_at', '>=', $state);
+                                    
+                                } elseif ($this->to) {
+                                    $query->where('created_at', '<=', $this->to);
+                                }
+                            });
+                        });}
+
+                    
+                    ),
+                    DateTimePicker::make('to')->label('To')->reactive(),
                     
                    
             
--- resources/views/filament/pages/top-visitor.blade.php
+++ resources/views/filament/pages/top-visitor.blade.php
@@ -24,6 +24,8 @@
         }
     </style>
  {{ $this->form }}
+ {{$from}}
+ {{$to}}
     <div class=" flex justify-end w-full b  ">
         <x-button rose spinner="exportToExcel" wire:click="exportToExcel" style="background: #03A340" icon="newspaper" class="mr-4">Export</x-button>
         <x-button rose spinner="print" wire:click="print" style="background: #03A340" icon="printer">Print</x-button>
@@ -48,20 +50,43 @@
             <thead>
                 <tr>
                     <th scope="col" class="py-2 pr-3 text-left text-xs font-semibold sm:pl-6" style="padding-left: 16px;">Name</th>
-                    <th scope="col" class="py-2 pr-3 text-left text-xs font-semibold sm:pl-6" style="padding-left: 16px;">Course</th>
                     <th scope="col" class="py-2 pr-3 text-left text-xs font-semibold sm:pl-6" style="padding-left: 16px;">Year</th>
+                    <th scope="col" class="py-2 pr-3 text-left text-xs font-semibold sm:pl-6" style="padding-left: 16px;">Course</th>
                     <th scope="col" class="py-2 pr-3 text-left text-xs font-semibold sm:pl-6" style="padding-left: 16px;">Campus</th>
                     <th scope="col" class="px-3 py-2 text-left text-xs font-semibold" style="padding-left: 16px;">Total Visits</th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-200">
+
+                @forelse($students as $student)
                 <tr>
-                    <td class="whitespace-normal py-2 pr-3 text-left text-xs font-medium" style="padding-left: 16px;">John Doe</td>
-                    <td class="whitespace-normal px-3 py-2 text-left text-xs" style="padding-left: 16px;">Information Teachnology</td>
-                    <td class="whitespace-normal px-3 py-2 text-left text-xs" style="padding-left: 16px;">1st Year</td>
-                    <td class="whitespace-normal px-3 py-2 text-left text-xs" style="padding-left: 16px;">Campus</td>
-                    <td class="whitespace-normal px-3 py-2 text-left text-xs" style="padding-left: 16px;">100</td>
+                    <td class="whitespace-normal py-2 pr-3 text-left text-xs font-medium" style="padding-left: 16px;">     {{ !empty($student) ? $student->last_name . ', ' . $student->last_name . ' '.$student->middle_name : '' }}
+                    </td>
+                   
+                    <td class="whitespace-normal px-3 py-2 text-left text-xs" style="padding-left: 16px;">
+
+                        {{ !empty($student) ? $student->year : '' }}
+                             
+                    </td>
+                   
+                    <td class="whitespace-normal px-3 py-2 text-left text-xs" style="padding-left: 16px;">
+
+                        {{ !empty($student) && !empty($student->course) ? $student->course->name : '   No Course Assigned' }}
+
+                             
+                    </td>
+                    <td class="whitespace-normal px-3 py-2 text-left text-xs" style="padding-left: 16px;">
+
+                        {{ !empty($student) && !empty($student->course) && !empty($student->course->campus) ? $student->course->campus->name : 'No Campus Assigned' }}
+
+                    
+                    </td>
+                    <td class="whitespace-normal px-3 py-2 text-left text-xs" style="padding-left: 16px;">{{$student->logins_count}}</td>
                 </tr>
+                @empty
+
+                @endforelse
+              
                
                 <!-- Add more rows with dynamic data here -->
             </tbody>
