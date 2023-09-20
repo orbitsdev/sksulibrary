e58ce42304bec6af7611ad8c1a7f321381e5be19
orbitsdev <orbinobrian0506@example.com>
2023-09-20 22:07:42 +0800

    wip

--- app/Filament/Widgets/AttenDanceChart.php
+++ app/Filament/Widgets/AttenDanceChart.php
@@ -16,35 +16,33 @@ class AttenDanceChart extends BarChartWidget
     protected int | string | array $columnSpan = 'full';
 
     
-
-        protected function getData(): array
-        {
-            // Get the data using a raw SQL query
-            $data = DB::table('day_records')
-                ->join('day_logins', 'day_records.id', '=', 'day_logins.day_record_id')
-                ->join('students', 'day_logins.student_id', '=', 'students.id')
-                ->join('courses', 'students.course_id', '=', 'courses.id')
-                ->select('courses.name as course_name', DB::raw('COUNT(day_logins.id) as login_count'))
-                ->whereYear('day_records.created_at', '=', Carbon::now()->year)
-                ->whereMonth('day_records.created_at', '=', Carbon::now()->month)
-                ->groupBy('courses.name')
-                ->get();
+    protected function getData(): array
+    {
+        // Get the data using a raw SQL query
+        $data = DB::table('day_records')
+            ->join('day_logins', 'day_records.id', '=', 'day_logins.day_record_id')
+            ->join('students', 'day_logins.student_id', '=', 'students.id')
+            ->join('courses', 'students.course_id', '=', 'courses.id')
+            ->select('courses.name as course_name', DB::raw('COUNT(DISTINCT students.id) as student_count'))
+            ->whereYear('day_records.created_at', '=', Carbon::now()->year)
+            ->whereMonth('day_records.created_at', '=', Carbon::now()->month)
+            ->groupBy('courses.name')
+            ->get();
     
-            $labels = $data->pluck('course_name')->toArray();
-            $loginCounts = $data->pluck('login_count')->toArray();
+        $labels = $data->pluck('course_name')->toArray();
+        $studentCounts = $data->pluck('student_count')->toArray();
     
-            return [
-                'datasets' => [
-                    [
-                        'backgroundColor' => '#0CE461',
-                        'label' => 'Number Of Students Visited Per Course',
-                        'data' => $loginCounts,
-                    ],
+        return [
+            'datasets' => [
+                [
+                    'backgroundColor' => '#0CE461',
+                    'label' => 'Number Of Students Visited Per Course',
+                    'data' => $studentCounts,
                 ],
-                'labels' => $labels,
-            ];
-        }
-
+            ],
+            'labels' => $labels,
+        ];
+    }
        
     
 }
