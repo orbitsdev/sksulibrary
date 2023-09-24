1ae48bce4446a967e81c37c876e6ea5113098c7d
orbitsdev <orbinobrian0506@example.com>
2023-09-24 23:28:43 +0800

    wip

--- resources/views/tables/columns/bar-code.blade.php
+++ resources/views/tables/columns/bar-code.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center justify-center">
+<div class="">
 
     <a href="{{route('barcode.download',['idNumber'=> $getRecord()->id_number])}}">
         {{-- <img src="data:image/png;base64,{{DNS1D::getBarcodePNG(str($getRecord()->id_number), 'S25+')}}" alt="barcode" /> --}}
